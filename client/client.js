c0065b12eb311cda7370eeb50fb8196fbd87e797
Kush Rastogi <kushrastogi@Kushs-MacBook-Pro.local>
2015-08-16 11:24:51 -0700

    Changed type of input from email to username in the login form

--- client/client.js
+++ client/client.js
@@ -1,7 +1,13 @@
 if (Meteor.isClient) {
   // counter starts at 0
-
+  Template.stop.events({
+    'keyup .stopname': function(){
+      var stopname = $(event.target).val();
+      var stopperson = Meteor.users.find().fetch();
+    }
+  })
   Template.main.events({
+    // Code to turn the static button images into a clickable "button"
     'mouseover .stop-button': function(){
       $('.stop-button').attr('src', 'assets/stophover.png');
     },
@@ -15,4 +21,8 @@ if (Meteor.isClient) {
       $('.stop-button').attr('src', 'assets/stop.png'); 
     }
   });
+  // Changes the email field in the login to a username
+  Accounts.ui.config({
+   passwordSignupFields: 'USERNAME_ONLY'
+});
 }
--- server/server.js
+++ server/server.js
@@ -2,4 +2,6 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+  Meteor.methods({
+  })
 }
